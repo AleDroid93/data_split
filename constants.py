9d9c895dacbe8449b80359c10325f7e55169be09
Alessandro Censi <alessandromic.censi@gmail.com>
2020-03-04 17:31:22 +0100

    Initial commit

---
+++ constants.py
@@ -0,0 +1,10 @@
+N_FOLDS = 5
+TRAIN_SIZE = 0.5
+VALID_SIZE = 0.2
+TEST_SIZE = 0.6
+
+DATA_PATH = 'data_reunion.npy'
+CLASSES_PATH = 'classes_reunion.npy'
+SEGMENT_ID_PATH = 'segment_id_reunion.npy'
+
+BASE_FOLDER_NAME = '../../Reunion-Dataset/fold '
---
+++ split.py
@@ -0,0 +1,44 @@
+import numpy as np
+import constants
+from sklearn import model_selection
+from sklearn.utils import shuffle
+from keras.utils import to_categorical
+
+
+
+
+classes = np.load(constants.CLASSES_PATH)
+data = np.load(constants.DATA_PATH)
+segments_id = np.load(constants.SEGMENT_ID_PATH)
+
+assert len(classes) == len(data) == len(segments_id)
+
+
+for i in range(constants.N_FOLDS):
+    # permutation of data
+    perm_data, perm_classes, perm_ids = shuffle(data, classes, segments_id, random_state=0)
+    perm_classes = to_categorical(perm_classes, num_classes=None)
+    files = []
+
+    x_train, x_test_validation, y_train, y_test_validation, id_train, id_test_valid = model_selection.train_test_split(perm_data, perm_classes, perm_ids,
+                                                                                              test_size=constants.TRAIN_SIZE,
+                                                                                              random_state=42, stratify=perm_classes)
+    x_validation, x_test, y_validation, y_test, id_valid, id_test = model_selection.train_test_split(x_test_validation,
+                                                                                    y_test_validation, id_test_valid,
+                                                                                    test_size=constants.TEST_SIZE, random_state=42, stratify=y_test_validation)
+
+
+    """ SAVING DATA INTO FILES """
+    out_path = constants.BASE_FOLDER_NAME + str(i+1) + "/"
+    np.save(out_path + "x_train_"+str(i+1), x_train)
+    np.save(out_path + "x_validation_"+str(i+1), x_validation)
+    np.save(out_path + "x_test_"+str(i+1), x_test)
+    np.save(out_path + "y_train_"+str(i+1), y_train)
+    np.save(out_path + "y_validation_"+str(i+1), y_validation)
+    np.save(out_path + "y_test_"+str(i+1), y_test)
+    np.save(out_path + "id_train_"+str(i+1), id_train)
+    np.save(out_path + "id_validation_"+str(i+1), id_valid)
+    np.save(out_path + "id_test_"+str(i+1), id_test)
+
+
+
